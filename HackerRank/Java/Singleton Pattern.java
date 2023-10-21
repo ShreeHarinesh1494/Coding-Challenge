711ebb297a8820d1e0741f2157c616576fd34d5a
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-10-21 22:03:12 +0530

    Create Singleton Pattern.java

---
+++ HackerRank/Java/Singleton Pattern.java
@@ -0,0 +1,15 @@
+Problem : Singleton Pattern
+
+Solution :
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print("Hello I am a singleton! Let me say "+str+" to you");
+    }
+}
