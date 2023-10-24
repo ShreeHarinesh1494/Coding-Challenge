8ee9c55f8b2e791585ce85436973fd2f56cb4c63
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-10-24 17:03:16 +0530

    Create Covariant Return Types.java

---
+++ HackerRank/Java/Covariant Return Types.java
@@ -0,0 +1,23 @@
+Problem : Covariant Return Types
+
+Solution :
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        if(s.equals("WestBengal"))
+        {
+            System.out.println("Jasmine");
+        }
+        else
+        {
+            System.out.println("Lily");
+        }
+    }
+}
